c3b5114fa20aa81dba6d716902ff4ecb908707f5
Harshdeep Singh <harshdeepsingh3009@gmail.com>
2018-07-26 11:58:57 -0400

    New Project Dashboard

---
+++ PhilipsHackathon-UI/src/app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+export var BlurAdmin = angular.module('BlurAdmin', [
+  'ngAnimate',
+  'ui.bootstrap',
+  'ui.sortable',
+  'ui.router',
+  'ngTouch',
+  'toastr',
+  'smart-table',
+  "xeditable",
+  'ui.slimscroll',
+  'ngJsTree',
+  'angular-progress-button-styles',
+
+  'BlurAdmin.theme',
+  'BlurAdmin.pages'
+]);
+
--- PhilipsHackathon-UI/src/main.ts
+++ PhilipsHackathon-UI/src/main.ts
@@ -1,12 +1,15 @@
+import './polyfills.ts';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { BlurAdmin } from './app/';
 import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(BlurAdmin)
   .catch(err => console.log(err));
